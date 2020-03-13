3a9c7481faaed89eacac94ba89d7dddda9b8b436
sv-dev2 <prince.chopra@kindlebit.com>
2020-03-13 11:51:36 +0530

    Add files via upload

---
+++ Insurance.Service.cs
@@ -0,0 +1,33 @@
+﻿using Insurance.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GensureAPIv2
+{
+    public class SummaryDetailService
+
+    {
+        public VehicleDetail GetVehicleInformation(int vehicleId)
+        {
+            var vehicle = InsuranceContext.VehicleDetails.Single(vehicleId);
+            return vehicle;
+        }
+        public Int32 getNewDebitNote()
+        {
+            var vehicle = InsuranceContext.SummaryDetails.Max("id");
+
+            if (vehicle != null)
+            {
+                return Convert.ToInt32(vehicle) + 1;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+
+    }
+}
